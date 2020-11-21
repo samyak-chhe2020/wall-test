61bc4b5e22a22286c9cfef7e32d01ea45f2c92b1
samyak-chhe2020 <72869523+samyak-chhe2020@users.noreply.github.com>
2020-11-21 16:54:54 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -7,7 +7,7 @@ function setup() {
   weight=random(30,52);
   thickness=random(22,83);
 
-  bullet=createSprite(50, 200, 20, 60);
+  bullet=createSprite(50, 200, 60, 20);
   wall=createSprite(1200,200,thickness,height/2);
   wall.shapeColor=color(80,80,80);
 
